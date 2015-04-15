3da5810674c46b8510fbbbbeaa79cd7f3d3d9a0a
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-04-16 02:10:57 +0300

    Extra path fixups.

--- shim/appengine_sdk/commands.py
+++ shim/appengine_sdk/commands.py
@@ -1,16 +1,25 @@
 def make_shim(script):
     def main():
-        import os
+        import os, sys
         global __name__
         global __file__
         global __path__
 
-        sdk_root = os.path.join(os.path.dirname(__file__), "google_appengine")
+        sdk_root = os.path.dirname(__file__)
+        sdk_path = os.path.join(sdk_root, "google_appengine")
         if 'GAE_SDK_ROOT' not in os.environ:
             os.environ['GAE_SDK_ROOT'] = sdk_root
-        __file__ = os.path.join(sdk_root, script)
+
+        # import paths fixup, just in case
+        if 'PYTHONPATH' in os.environ:
+            os.environ['PYTHONPATH'] += os.pathsep + sdk_path
+        else:
+            os.environ['PYTHONPATH'] = sdk_path
+        sys.path = [sdk_path] + sys.path
+
+        __file__ = sys.argv[0] = os.path.join(sdk_path, script)
         __name__ = "__main__"
-        del os, sdk_root
+        del os, sdk_root, sys
 
         execfile(__file__, globals(), globals())
     return main
